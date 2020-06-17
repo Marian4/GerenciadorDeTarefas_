d4ea271b549b567f01fe463c7f31beb1830df74b
Mariana Moraes <mraes.mariana@gmail.com>
2020-06-17 18:32:28 -0300

    Página Not Found

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/NotFound.css'
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="NotFound">
+                    <div className="mensagem">Página Não encontrada</div>
+            </div>
+        )
+    }
+}
+
+export default Home;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './components/Home';
 import Cadastro from './components/Cadastro';
 import Tarefas from './components/Tarefas';
+import NotFound from './components/NotFound';
 
 import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
@@ -11,6 +12,8 @@ const Routes = () => (
             <Route exact path="/" component={() => <Home/>}/>
             <Route exact path="/register" component={() => <Cadastro/>}/>
             <Route exact path="/tasks" component={() => <Tarefas/>}/>
+            <Route path="/404" component={() => <NotFound/>} />
+            <Redirect to="/404" />
         </Switch>
     </BrowserRouter>
 );
